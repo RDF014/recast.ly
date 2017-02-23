5d1170c9be03ca869aaafeaa56a255654b90b840
rdf014 <rdf014@gmail.com>
2017-02-23 12:39:01 -0800

    working on youtube api

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,16 @@
 var searchYouTube = (options, callback) => {
-  // TODOs
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: JSON.stringify(Object),
+    success: function (data) {
+      console.log('Successs!', data);  
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('YouTube: Failed to receive message', data);
+    }
+  });// TODOs
 };
 
 window.searchYouTube = searchYouTube;
