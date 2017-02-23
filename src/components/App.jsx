d4da33cb0e8b9af3d3c7bbe37dee2e50a45c8361
rdf014 <rdf014@gmail.com>
2017-02-22 21:14:28 -0800

    Working on implementing click on app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,43 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component { 
+  constructor(props) {
+    super(props);
+
+    this.currentVideo = exampleVideoData[0];
+    this.state = {
+      done: false
+    };
+    this.onListItemClick = this.onListItemClick.bind(this);
+  }
+  onListItemClick() {
+    console.log('this', this);
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  handleClick(e) {
+    // console.log( 'this is:', this);
+  }
+
+  render() {
+    // console.log('video', this.currentVideo);
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={window.exampleVideoData} videoChange={this.onListItemClick}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,25 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+var VideoListEntry = (props) => {
+  const onClickListItem = event => {
+    // debugger;
+    App.prototype.currentVideo = props.video;
+    // App.prototype.render();
+    // App.prototype.onListItemClick();
+    console.log(props);
+    props.videoChange();
+  };
+
+  return (
+    <div className="video-list-entry">
+      <div className="media-left media-middle">
+        <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
+      </div>
+      <div className="media-body">  
+        <div className="video-list-entry-title" onClick={onClickListItem}>{props.video.snippet.title}</div>
+        <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
